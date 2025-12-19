1621517f13dcc8845745a1c90a593073bafe5575
Hector Talavera <htalaveraj@gmail.com>
2025-12-19 13:36:19 -0600

    first commit

---
+++ src/main/java/com/io/usyc/UsycServicesApplication.java
@@ -0,0 +1,13 @@
+package com.io.usyc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsycServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsycServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/io/usyc/UsycServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.usyc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsycServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
